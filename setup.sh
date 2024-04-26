457bf497f163e9e0fc316ab85afe574067fe48bd
Giona2 <153511987+Giona2@users.noreply.github.com>
2024-04-26 13:55:02 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
 
+configdir=/home/$USER/.config
+repodir=/home/$USRE/TermuxApps
+
+mkdir $configdir/tpm
+mkdir $configdir/tpm/config
+
+mv $repodir/tpm $configdir/tpm
+mv $repodir/about.toml $configdir/tpm
+mv $repodir/bin $configdir/tpm
+
+rm $repodir/README.md
+rm $repodir/setup.sh
+rm $repodir
